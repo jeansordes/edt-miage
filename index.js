e4a1fb50b5f88a8dd70351db2da46c9c1b489fc6
Jean Sordes <john.sordes@gmail.com>
2020-01-15 00:20:30 +0100

    html files are not generated by the script anymore

--- index.js
+++ index.js
@@ -19,47 +19,6 @@ const workOffline = getArgvValue('offline').argFound;
 // (sauf si on est offline, dans ce cas là, balek)
 if (!workOffline && !mail.isSetupGood()) return mail.logerrAuth();
 
-// Vérifie si on a de quoi générer le fichier index.html
-const prodServerURLArg = getArgvValue('prodServerURL');
-let prodServerURL = prodServerURLArg.argFound ? prodServerURLArg.string : 'http://edt.miage.online';
-// si l'URL du serveur ne termine pas par un / on en rajoute un
-if (prodServerURL.slice(-1) != '/') prodServerURL += '/';
-
-let htmlContent = `<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Edt Miage</title><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"><style>body{font-family: 'Roboto', sans-serif} iframe{background-color: lightgray} input {min-width: 50%}</style><script async src="https://www.googletagmanager.com/gtag/js?id=UA-155402665-1"></script><script>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-155402665-1");</script>
-<script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
-<meta property="og:title"              content="Emplois du temps de la Miage promo 2022 à Toulouse" />
-<meta property="og:description"        content="Pour pouvoir consulter en un clin d'oeil votre emploi du temps 💪🏻" />
-<meta property="og:image"              content="${prodServerURL}preview.png" />
-<style>
-body {
-    white-space: nowrap;
-}
-body>span {
-    white-space: initial;
-    display: inline-block;
-    margin-right: 2rem;
-    max-width: 250px;
-}</style></head><body>
-    <span>
-        <p>Emploi du temps UT3 Paul Sabatier</p>
-        <p><input onClick="this.select();" id="ut3" value="${prodServerURL}ut3.ics" type="text"/><button class="copybtn" data-clipboard-target="#ut3">copier URL</button></p>
-        <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut3.ics&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>
-    </span>
-    <span>
-        <p>Emploi du temps UT1 Capitole (<strong>Formation en Alternance</strong>)</p>
-        <p><input onClick="this.select();" id="ut1fa" value="${prodServerURL}ut1_fa.ics" type="text"/><button class="copybtn" data-clipboard-target="#ut1fa">copier URL</button></p>
-        <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1_fa.ics&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>
-    </span>
-    <span>
-        <p>Emploi du temps UT1 Capitole (<strong>Formation Initiale</strong>)</p>
-        <p><input onClick="this.select();" id="ut1fi" value="${prodServerURL}ut1_fi.ics" type="text"/><button class="copybtn" data-clipboard-target="#ut1fi">copier URL</button></p>
-        <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1_fi.ics&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>
-    </span>
-    <p>site réalisé par <a href="https://www.jzs.fr/">Jean Sordes</a></p>
-<script>new ClipboardJS('.copybtn')</script>
-</body></html>`;
-fs.writeFileSync(paths.indexHtml, htmlContent);
-
 // UT1
 const whenUT1Done = (filiere) => console.log(`✅  UT1 ${filiere} File is ready`);
 if (workOffline) {
--- src/util/parseArgv.js
+++ src/util/parseArgv.js
@@ -4,7 +4,7 @@ module.exports = {
     printUsage: () => {
         console.log('\x1b[36m%s\x1b[0m', 'Usage:\n\t'
             + 'node index.js --offline (use only the files already downloaded, only for dev purposes)' + '\n\t'
-            + 'node index.js --prodServerURL="https://edt.miage.online" --mailHostAdress="smtp.server.fr" --mailHostPort="587" --personToContact="john@doe.fr" --emailLogin="user.name" --emailPassword="password"\n\n'
+            + 'node index.js --mailHostAdress="smtp.server.fr" --mailHostPort="587" --personToContact="john@doe.fr" --emailLogin="user.name" --emailPassword="password"\n\n'
             + 'Options : [--ut3pdf2svg : force le script a regénérer le fichier SVG à partir du fichier PDF de l\'UT3]');
     },
     getArgvValue: fieldName => {
