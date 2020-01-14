2254fb2a82d2efb0ec377e2572a7684636684347
Jean Sordes <john.sordes@gmail.com>
2020-01-14 23:17:48 +0100

    "copier URL" button added

--- index.js
+++ index.js
@@ -26,32 +26,34 @@ let prodServerURL = prodServerURLArg.argFound ? prodServerURLArg.string : 'https
 if (prodServerURL.slice(-1) != '/') prodServerURL += '/';
 
 let htmlContent = `<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Edt Miage</title><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"><style>body{font-family: 'Roboto', sans-serif} iframe{background-color: lightgray} input {min-width: 50%}</style><script async src="https://www.googletagmanager.com/gtag/js?id=UA-155402665-1"></script><script>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-155402665-1");</script>
+<script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
 <style>
 body {
     white-space: nowrap;
 }
-body>div {
+body>span {
     white-space: initial;
     display: inline-block;
     margin-right: 2rem;
     max-width: 250px;
 }</style></head><body>
-    <div>
+    <span>
         <p>Emploi du temps UT3 Paul Sabatier</p>
-        <p><input onClick="this.select();" value="${prodServerURL}ut3.ics" type="text"/></p>
+        <p><input onClick="this.select();" id="ut3" value="${prodServerURL}ut3.ics" type="text"/><button class="copybtn" data-clipboard-target="#ut3">copier URL</button></p>
         <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut3.ics&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>
-    </div>
-    <div>
+    </span>
+    <span>
         <p>Emploi du temps UT1 Capitole (<strong>Formation en Alternance</strong>)</p>
-        <p><input onClick="this.select();" value="${prodServerURL}ut1_fa.ics" type="text"/></p>
+        <p><input onClick="this.select();" id="ut1fa" value="${prodServerURL}ut1_fa.ics" type="text"/><button class="copybtn" data-clipboard-target="#ut1fa">copier URL</button></p>
         <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1_fa.ics&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>
-    </div>
-    <div>
+    </span>
+    <span>
         <p>Emploi du temps UT1 Capitole (<strong>Formation Initiale</strong>)</p>
-        <p><input onClick="this.select();" value="${prodServerURL}ut1_fi.ics" type="text"/></p>
+        <p><input onClick="this.select();" id="ut1fi" value="${prodServerURL}ut1_fi.ics" type="text"/><button class="copybtn" data-clipboard-target="#ut1fi">copier URL</button></p>
         <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1_fi.ics&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>
-    </div>
+    </span>
     <p>site réalisé par <a href="https://www.jzs.fr/">Jean Sordes</a></p>
+<script>new ClipboardJS('.copybtn')</script>
 </body></html>`;
 fs.writeFileSync(paths.indexHtml, htmlContent);
 
