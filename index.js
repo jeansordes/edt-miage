ef5c0087e7652b166a575b64735ee5832a4b4df2
Jean Sordes <john.sordes@gmail.com>
2020-01-14 22:20:20 +0100

    Pour rester rétrocompatible avec les gens qui utilisaient ut1.ics on créé une copie du fichier pour que les gens qui utilisent l'ancien fichier puisse encore le faire (ut1.ics == ut1_fa.ics)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const
     pdf2svg = async (inputPath, outputPath) => await exec(`inkscape -l "${outputPath}" "${inputPath}"`),
     { getArgvValue } = require('./src/util/parseArgv'),
     fs = require('fs'),
+    path = require('path'),
     mail = require('./src/util/mail');
 
 // Vérifier si l'utilisateur a lancé le programme en mode "offline"
@@ -27,13 +28,16 @@ if (!workOffline) {
 
         let htmlContent = `<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Edt Miage</title><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"><style>body{font-family: 'Roboto', sans-serif} iframe{background-color: lightgray} input {min-width: 50%}</style><script async src="https://www.googletagmanager.com/gtag/js?id=UA-155402665-1"></script><script>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-155402665-1");</script></head><body>
     <h3>Liens vers les fichiers iCal (.ics)</h3>
-    <p><input onClick="this.select();" value="${prodServerURL}ut1.ics" type="text"/></p>
+    <p><input onClick="this.select();" value="${prodServerURL}ut1_fa.ics" type="text"/></p>
+    <p><input onClick="this.select();" value="${prodServerURL}ut1_fi.ics" type="text"/></p>
     <p><input onClick="this.select();" value="${prodServerURL}ut3.ics" type="text"/></p>
     <h3>Aperçu des emplois du temps</h3>
     <p>Emploi du temps UT3 Paul Sabatier</p>
     <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut3.ics&theme=RE&ccolor=%23ffffc0&dims=1&gtype=cv_monthgrid&gcloseable=0&gnavigable=1&gperiod=month&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=800></iframe>
-    <p>Emploi du temps UT1 Capitole</p>
-    <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1.ics&theme=RE&ccolor=%23ffffc0&dims=1&gtype=cv_monthgrid&gcloseable=0&gnavigable=1&gperiod=month&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=800></iframe>
+    <p>Emploi du temps UT1 Capitole (<strong>Formation en Alternance</strong>)</p>
+    <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1_fa.ics&theme=RE&ccolor=%23ffffc0&dims=1&gtype=cv_monthgrid&gcloseable=0&gnavigable=1&gperiod=month&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=800></iframe>
+    <p>Emploi du temps UT1 Capitole (<strong>Formation Initiale</strong>)</p>
+    <iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=${encodeURIComponent(prodServerURL)}ut1_fi.ics&theme=RE&ccolor=%23ffffc0&dims=1&gtype=cv_monthgrid&gcloseable=0&gnavigable=1&gperiod=month&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=800></iframe>
     </body></html>`;
         fs.writeFileSync(paths.indexHtml, htmlContent);
     }
@@ -46,6 +50,9 @@ if (workOffline) {
     whenUT1Done('FI');
 } else {
     saveUT1file(paths.ut1_fa.url, paths.ut1_fa.ics_tmp, paths.ut1_fa.ics).then(({ isNew, time }) => {
+        // Pour éviter de casser l'ancien fonctionnement, on créé une copie de ut1_fa.ics, et on le renomme ut1_fa.ics
+        fs.copyFileSync(paths.ut1_fa.ics, path.resolve('./assets/ut1.ics'));
+
         whenUT1Done('FA');
         console.log(isNew ? "New file :" : "Already up-to-date :", time);
     });
