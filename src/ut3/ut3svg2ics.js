69ca03d33f61cfdb49f02b9ed9eee13e2b51300f
Jean Z. SORDES <john.sordes@gmail.com>
2020-09-03 02:17:46 +0200

    aout 2020

--- src/ut3/ut3svg2ics.js
+++
@@ -1,7 +0,0 @@
-const
-    svg2txt = require('./svg2txt'),
-    txt2ics = require('./txt2ics');
-
-module.exports = async (svgPath, icsPath, fileCreatedOn) => await svg2txt(svgPath).then(evts => {
-    txt2ics(evts, icsPath, fileCreatedOn);
-});
